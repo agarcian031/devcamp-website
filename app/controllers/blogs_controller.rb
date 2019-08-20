3ee29180fd9ce0e1ac960f08965cbbd9b05fada4
Amanda Garcia <algarcian@outlook.com>
2019-08-20 14:07:52 -0500

    scaffold for blogs

---
+++ app/controllers/blogs_controller.rb
@@ -0,0 +1,79 @@
+class BlogsController < ApplicationController
+  before_action :set_blog, only: [:show, :edit, :update, :destroy]
+  # calls the method set_blog only for show, edit, update, and destroy. 
+
+  # GET /blogs
+  # GET /blogs.json
+  def index
+    @blogs = Blog.all
+  end
+
+  # GET /blogs/1
+  # GET /blogs/1.json
+  def show
+    # @blog = Blog.find(params[:id])
+  end
+
+  # GET /blogs/new
+  # Doesn't actually create a new blog, it just gives you the view page to display the create form
+  def new
+    @blog = Blog.new
+  end
+
+  # GET /blogs/1/edit
+  def edit
+  end
+
+  # POST /blogs
+  # POST /blogs.json
+  # is actually taking the parameters of the blog and creating the blog and saving it in the database. 
+  # once it create the blog it will render the show page for the blog
+  def create
+    @blog = Blog.new(blog_params)
+
+    respond_to do |format|
+      if @blog.save
+        format.html { redirect_to @blog, notice: 'Blog was successfully created.' }
+        format.json { render :show, status: :created, location: @blog }
+      else
+        format.html { render :new }
+        format.json { render json: @blog.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /blogs/1
+  # PATCH/PUT /blogs/1.json
+  def update
+    respond_to do |format|
+      if @blog.update(blog_params)
+        format.html { redirect_to @blog, notice: 'Blog was successfully updated.' }
+        format.json { render :show, status: :ok, location: @blog }
+      else
+        format.html { render :edit }
+        format.json { render json: @blog.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /blogs/1
+  # DELETE /blogs/1.json
+  def destroy
+    @blog.destroy
+    respond_to do |format|
+      format.html { redirect_to blogs_url, notice: 'Blog was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_blog
+      @blog = Blog.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def blog_params
+      params.require(:blog).permit(:title, :body)
+    end
+end
