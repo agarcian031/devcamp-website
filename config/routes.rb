8165c31372ea3d44d572b5063b89c031dacce6dc
Amanda Garcia <algarcian@outlook.com>
2019-08-20 14:18:54 -0500

    add rails routes  overview

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  # resources encapsulates all of the routes that the model uses
+  # is it called RESTful routing
   resources :blogs
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
