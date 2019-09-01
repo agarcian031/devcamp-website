e7ad51937141165b81bc9ffb58186901a187c312
Amanda Garcia <algarcian@outlook.com>
2019-09-01 13:20:41 -0500

    Built pages via controller generator

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'pages/home'
+  get 'pages/about'
+  get 'pages/contact'
   # resources encapsulates all of the routes that the model uses
   # is it called RESTful routing
   resources :blogs
