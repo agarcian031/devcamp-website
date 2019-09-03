43cad0a9d03c09b1bed541b506b88d9c30826a93
Amanda Garcia <algarcian@outlook.com>
2019-09-03 17:03:35 -0500

    build the new and create actions for portfolios controller

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,11 +1,27 @@
 class PortfoliosController < ApplicationController
 
   def index 
-    @portfolio_items = Portfolio.all.order('created_at desc')
+    @portfolio_items = Portfolio.all
+  end 
+ 
+
+  def new 
+    @portfolio_item = Portfolio.new 
   end 
 
-  def show 
-  end 
+  def create
+    @portfolio_item = Portfolio.new(portfolio_params)
+
+    respond_to do |format|
+      if @portfolio_item.save
+        format.html { redirect_to portfolios_path, notice: 'Portfolio was successfully created.' }
+      else
+        format.html { render :new }
+      
+      end
+    end
+  end
+
 
   def edit 
   end 
@@ -14,5 +30,10 @@ class PortfoliosController < ApplicationController
   end 
 
   def destroy 
+  end 
+
+  private 
+  def portfolio_params 
+    params.require(:portfolio).permit(:title, :subtitle, :body)
   end 
 end
