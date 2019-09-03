07b0bc49599dfb2d4e56a23cc953e922bbe1f191
Amanda Garcia <algarcian@outlook.com>
2019-09-03 16:07:37 -0500

    integrated porfolio index action and view

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,2 +1,18 @@
 class PortfoliosController < ApplicationController
+
+  def index 
+    @portfolio_items = Portfolio.all.order('created_at desc')
+  end 
+
+  def show 
+  end 
+
+  def edit 
+  end 
+
+  def update 
+  end 
+
+  def destroy 
+  end 
 end
