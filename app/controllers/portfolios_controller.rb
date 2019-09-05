7812eb9e47f31ac04cb98d5eae8391348235748a
Amanda Garcia <algarcian@outlook.com>
2019-09-05 15:51:40 -0500

    Edit controller for portfolio

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,4 +1,5 @@
 class PortfoliosController < ApplicationController
+  before_action :set_portfolio, only: [:show, :edit, :update, :destroy]
 
   def index 
     @portfolio_items = Portfolio.all
@@ -27,13 +28,33 @@ class PortfoliosController < ApplicationController
   end 
 
   def update 
+
+    respond_to do |format|
+      if @portfolio.update(portfolio_params)
+        format.html { redirect_to @portfolio, notice: 'Portfolio was successfully updated. '}
+        format.json { render :show, status: :ok, location: @portfolio}
+      else 
+        format.html {render :edit}
+        format.json {render json: @portfolio.errors, status: :unprocessable_entity}
+      end 
   end 
+end 
 
   def destroy 
+    @portfolio.destroy 
+    respond_to do |format| 
+      format.html { redirect_to portfolios_url, notice: 'Portfolio was removed. '}
+      format.json { head :no_content}
+    end 
+
   end 
 
   private 
   def portfolio_params 
     params.require(:portfolio).permit(:title, :subtitle, :body)
+  end 
+
+  def set_portfolio
+    @portfolio = Portfolio.find(params[:id])
   end 
 end
