1b4cbab80e9924bc0bb7447c16a81aa79a3168e0
Amanda Garcia <algarcian@outlook.com>
2019-09-06 12:46:51 -0500

    Set up enums for if blog is draft or published

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -2,4 +2,8 @@ class Blog < ApplicationRecord
   # will replace id with title slug 
   extend FriendlyId
   friendly_id :title, use: :slugged
+
+  # enum 
+  # 0 will be mapped to draft so whenever we get a new blog post, it will automatically be counted as a draft 
+  enum status: { draft: 0, published: 1}
 end
