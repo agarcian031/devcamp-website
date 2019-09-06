c339756e6be2339d86c31e1bebbdf8a4286ab6c8
Amanda Garcia <algarcian@outlook.com>
2019-09-06 10:56:19 -0500

    Set up root and custom routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
-  resources :portfolios
-  get 'pages/home'
-  get 'pages/about'
-  get 'pages/contact'
+  root to: 'pages#home'
+  # get 'pages/home'
+  get 'about', to: 'pages#about'
+  get 'contact', to: 'pages#contact'
   # resources encapsulates all of the routes that the model uses
   # is it called RESTful routing
   resources :blogs
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :portfolios
+
 end
