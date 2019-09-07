b3501301b7ca419ff8e798d8fddced65b6d83edd
Amanda Garcia <algarcian@outlook.com>
2019-09-07 13:01:23 -0500

    added image defaults in portfolio model

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -9,4 +9,16 @@ class Portfolio < ApplicationRecord
   # custom scope using scope keyword 
   # lambda - is a way of encapsulating an entire process in Ruby 
   scope :ror_items,  -> {  where(subtitle: 'Ruby on Rails')}
+
+  # data defaults 
+  # after a portfolio item has been initialized (when new action is called and form is generated), this will automatically run  
+  after_initialize :set_defaults 
+
+  # ||= if the image is nil, then set to the image to the value passes in 
+  
+  def set_defaults
+    self.main_image ||= "https://via.placeholder.com/600x400"
+    self.thumb_image ||= "https://via.placeholder.com/350x200"
+  end 
 end
+
