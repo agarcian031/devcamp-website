1eb6c097dc5583883ef808de1b1b0bdcb3b027bd
Amanda Garcia <algarcian@outlook.com>
2019-09-14 14:07:14 -0500

    implemented custom registration, login, and logout routes for devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
+  # custom paths for devise instead of having /users/sign_up etc.  
+  devise_for :users, path: '', path_names: {sign_in: 'login', sign_out: 'logout', sign_up: 'register'}
   root to: 'pages#home'
   # get 'pages/home'
   get 'about', to: 'pages#about'
