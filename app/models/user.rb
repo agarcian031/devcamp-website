730560ae4428927c6b7c8da17b632e487ba8444b
Amanda Garcia <algarcian@outlook.com>
2019-09-14 15:35:49 -0500

    virtual attribute methods for first name and last name in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,16 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable, :trackable,
          :recoverable, :rememberable, :validatable
+# makes the name required so that it won't give an error when it tries to get the name and run the following methods 
+validates_presence_of :name 
+  # virtual attributes 
+
+  # Will split the users name attribute and will take the first name in its own array for later use 
+  def first_name 
+    self.name.split.first 
+  end 
+
+  def last_name 
+    self.name.split.last 
+  end 
 end
