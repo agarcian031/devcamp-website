99c9438dba5f0f535fcbe273e514a9489548b387
Amanda Garcia <algarcian@outlook.com>
2019-09-16 18:11:29 -0500

    setting up sessions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,14 @@ class ApplicationController < ActionController::Base
   include DeviseWhitelist 
 
 
+  # Sessions 
+  # Don't put secure information in sessions. 
+  before_action :set_source 
+
+  def set_source
+    # will set q but only if it is available in params. 
+    session[:source] = params[:q] if params[:q]
+  end 
+
+
 end
